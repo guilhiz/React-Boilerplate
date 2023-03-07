f1ac0bc9a484f50248e3fda58daa85b55b294923
guilhiz <guilhermevilelasobral@gmail.com>
2023-03-06 23:20:59 -0300

    feat: add styled-components theme provider configuration

--- src/@types/styled-component.d.ts
+++ src/@types/styled-component.d.ts
@@ -3,5 +3,6 @@ import { defaultTheme } from '../theme';
 type CustomTheme = typeof defaultTheme;
 
 declare module "styled-components" {
+  // @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends CustomTheme {}
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from '../theme';
+import GlobalStyle from '../styles/GlobalStyle';
 
 function App() {
   return (
-    <div>
-      <div>Ola mundo</div>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>
+        <div>Ola mundo</div>
+      </div>
+    </ThemeProvider>
   );
 }
 
