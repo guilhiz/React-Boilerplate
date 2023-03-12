da07572d1f0fb95b3e77b8f05d8ab4197ecaf4c8
guilhiz <guilhermevilelasobral@gmail.com>
2023-03-11 21:24:16 -0300

    feat: add example page to make pages folder visible

---
+++ src/pages/PageExample/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+const PageExample = () => {
+  return (
+    <S.Container>
+      <S.Content></S.Content>
+    </S.Container>
+  );
+};
+
+export default PageExample;
