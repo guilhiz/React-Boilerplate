97a390d71c773e42c978cf4684e84a3392020885
guilhiz <guilhermevilelasobral@gmail.com>
2023-03-22 08:29:06 -0300

    feat: migrate project from CRA to Vite
    Migrated project from Create React App (CRA) to Vite. Updated tsconfig and package.json as required.
    Updated build and development scripts in package.json. Removed unnecessary dependencies and
    configurations. Tested and verified that the application is working correctly.

--- src/pages/PageExample/styles.ts
+++ src/pages/PageExample/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 3rem;
+  > img {
+    display: block;
+    height: 100px;
+  }
+`;
