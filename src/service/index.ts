2397c3cc973600f2d556aaf9840fdb20520d8d41
guilhiz <guilhermevilelasobral@gmail.com>
2023-05-04 18:28:38 -0300

    feat: add .env.local.example

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { ParamsProps } from './types';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 const authConfig = (token: string) => ({
